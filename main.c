211aa98ff3dd68cd4f315f745947514820e2f55b
eunso <55428135+eunso@users.noreply.github.com>
2019-10-08 21:44:52 +0900

    05_control_05

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
 	int i; 
 	for ( i=0; i<10; i++ ) 
 	printf("Hello World!\n");
